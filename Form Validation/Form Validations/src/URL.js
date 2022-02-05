a204c2638f91194ebeca46cb3ad543201357cc7a
Luka Sharashenidze <lsharashenize@gmail.com>
2022-02-05 20:34:55 +0400

    Form Validation

--- Form Validation/Form Validations/src/URL.js
+++
@@ -1,1 +0,0 @@
-export const URL = "http://localhost:1337/api/forms";
--- Form Validation/Test Api With Strapi/config/admin.js
+++
@@ -1,5 +0,0 @@
-module.exports = ({ env }) => ({
-  auth: {
-    secret: env('ADMIN_JWT_SECRET', '94e3ecf941362a66531181848f478476'),
-  },
-});

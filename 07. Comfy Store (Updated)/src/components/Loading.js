24c5a00bf08a7b0b558791777c5c2e277843d2c9
Luka Sharashenidze <lsharashenidze2001@gmail.com>
2024-03-16 13:55:17 +0400

    r

--- 07. Comfy Store (Updated)/src/components/Loading.js
+++
@@ -1,9 +0,0 @@
-const Loading = () => {
-  return (
-    <article className="loading-container">
-      <div className="loading"></div>
-    </article>
-  );
-};
-
-export default Loading;
--- 07. Comfy Store (Updated)/src/pages/About.js
+++
@@ -1,7 +0,0 @@
-import AboutComponent from "../components/About";
-
-const About = () => {
-  return <AboutComponent />;
-};
-
-export default About;

94890a61a097e88a5ff2e95285879efab52b775d
Luka Sharashenidze <lsharashenidze2001@gmail.com>
2024-03-16 14:07:26 +0400

    r

--- 07. Comfy Store (Updated)/src/helpers/formatPrice.js
+++
@@ -1,7 +0,0 @@
-export const formatPrice = price => {
-  const formattedPrice = new Intl.NumberFormat("en-us", {
-    style: "currency",
-    currency: "USD",
-  }).format(price / 100);
-  return formattedPrice;
-};

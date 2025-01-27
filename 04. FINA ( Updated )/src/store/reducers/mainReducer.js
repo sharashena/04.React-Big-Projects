76f3e01ce878f42eef8859286287d48fa73ac7d0
Luka Sharashenidze <lsharashenidze2001@gmail.com>
2025-01-28 02:42:38 +0400

    Delete Projects

--- 04. FINA ( Updated )/src/store/reducers/mainReducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-// reducers
-import { todoReducer } from "./todoReducer";
-import { treeGridReducer } from "./treeGridReducer";
-
-export const combine = combineReducers({
-  todoState: todoReducer,
-  treeGridState: treeGridReducer,
-});

2a6e2bc7469745bc6233270e869f794292761deb
Luka Sharashenidze <lsharashenidze2001@gmail.com>
2025-01-28 02:41:39 +0400

    Delete Project

--- 06. Fakeflix (Uncompleted)/src/pages/AllPage.js
+++
@@ -1,8 +0,0 @@
-import Home from "./Home";
-import TVSeries from "./TVSeries";
-import Movies from "./Movies";
-import Popular from "./Popular";
-import Mylist from "./Mylist";
-import Error from './Error'
-
-export { Home, TVSeries, Movies, Popular, Mylist, Error };
--- 06. Fakeflix (Uncompleted)/src/redux store/store.js
+++
@@ -1,5 +0,0 @@
-import { createStore } from "redux";
-import { combine } from "./reducers/combine";
-import { composeWithDevTools } from "redux-devtools-extension";
-
-export const store = createStore(combine, composeWithDevTools());
